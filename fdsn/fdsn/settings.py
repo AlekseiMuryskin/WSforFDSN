81a97337c79bf952f1810469b63b830285e2140a
aleksey <muryskinas@yandex.ru>
2022-04-19 14:55:15 +0500

    Добавлен адрес 172.20.1.140. Ну и звездочка на всякий случай

--- fdsn/fdsn/settings.py
+++ fdsn/fdsn/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^6q@(rxp!efup5%5my7$8$3^qsq6ahz=5068ygvzm2!t=e4*$*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1","192.168.35.94"]
+ALLOWED_HOSTS = ["127.0.0.1","192.168.35.94","*","172.20.1.140"]
 
 
 # Application definition
